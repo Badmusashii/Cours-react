53a4c8cb9124d627116b4525741d571cc7c05c8a
KILINC Bayram <badmusashii@gmail.com>
2023-01-17 13:16:06 +0100

    1er sauvegarde en branche

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navigation from "../compenents/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eaque
+        consectetur laudantium minima numquam quasi.
+      </p>
+    </div>
+  );
+};
+
+export default About;
