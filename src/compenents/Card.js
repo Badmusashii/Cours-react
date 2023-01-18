7607c3d7fa0d690f9bcf80e2ab41e74f11aaea65
KILINC Bayram <badmusashii@gmail.com>
2023-01-18 12:58:35 +0100

    Projet terminer

--- src/compenents/Card.js
+++ src/compenents/Card.js
@@ -1,11 +1,20 @@
+import { object } from "prop-types";
 import React from "react";
 
 const Card = ({ country }) => {
+  console.log(country);
+
   return (
     <li className="card">
-      <img src={country.flags.png} alt="" />
+      <img
+        src={country.flags.png}
+        alt={"Drapeau " + country.translations.fra.common}
+      />
       <div className="infos">
-        <h2>Nom du pays</h2>
+        <h2>{country.translations.fra.common}</h2>
+        <h3> Capital : {country.capital}</h3>
+        <p>{country.population.toLocaleString()} habitants</p>
+        {/* .toLocaleString sert à integrer un separateur de milliers dans le nombres des habitants */}
       </div>
     </li>
   );
